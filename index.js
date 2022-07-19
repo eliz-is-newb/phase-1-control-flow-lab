ac249c4b9b728bd731c12e34baff0283b815d840
elizabeth sky degrate <skydegrate@gmail.com>
2022-07-18 22:52:19 -0400

    commit tree 85fb52026b9cfdf38e679d5f95d2cfd98f484ba1

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+    if(distance <= 400){
+    return 'This one is on me!';
+  } else if(distance > 400 && distance <= 2000){
+    return 'That will be twenty bucks.';
+  } else if(distance > 2000 && distance <= 2500){ 
+    return 'I will gladly take your thirty bucks.';
+
+  } else if(distance > 2500){ 
+    return 'No can do.'
+  }
+
+  
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(location){
   // Write your code here!
+  if(location === "NYC"){
+    return 'Ok, sounds good.'
+  } else if(location !== "NYC"){
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if(tip === "generous"){
+    return "Thank you so much."
+  } else if(tip === "not as generous"){
+    return "Thank you."
+  } else if(tip === "thanks for everything"){ 
+    return "Bye."
+  }
+
 }
